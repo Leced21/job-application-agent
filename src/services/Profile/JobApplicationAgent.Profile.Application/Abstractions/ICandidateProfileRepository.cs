21993329e25de1a946ed3b7d583eb16ee004224d
cedric <cedricnzia@gmail.com>
2026-09-19 12:20:34 +0200

    init_project

---
+++ src/services/Profile/JobApplicationAgent.Profile.Application/Abstractions/ICandidateProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using JobApplicationAgent.Profile.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobApplicationAgent.Profile.Application.Abstractions
+{
+    public interface ICandidateProfileRepository
+    {
+        Task<CandidateProfile?> GetAsync(CancellationToken cancellationToken = default);
+
+        Task AddAsync(CandidateProfile profile, CancellationToken cancellationToken = default);
+
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/services/Profile/JobApplicationAgent.Profile.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using JobApplicationAgent.Profile.Application.Abstractions;
+using JobApplicationAgent.Profile.Infrastructure.Persistence;
+using JobApplicationAgent.Profile.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobApplicationAgent.Profile.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("ProfileDatabase") ?? throw new InvalidOperationException("Connection string 'ProfileDatabase' was not found.");
+
+            services.AddDbContext<ProfileDbContext>(options => options.UseNpgsql(connectionString));
+
+            services.AddScoped<ICandidateProfileRepository, CandidateProfileRepository>();
+
+            return services;
+        }
+    }
+}
